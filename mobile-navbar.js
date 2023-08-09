4118a4a70832e0e639c288a8c526560bc7cad6e6
Ana Clara Maciel <130106962+maciel-ana@users.noreply.github.com>
2023-08-09 17:09:39 -0300

    commit maciel

--- mobile-navbar.js
+++ mobile-navbar.js
@@ -8,19 +8,11 @@ class MobileNavbar {
         this.handleClick = this.handleClick.bind(this);
     }
 
-    animateLinks(){
-        this.navLinks.forEach((link) => {
-            link.style.animation
-            ? (link.style.animation = "") 
-            : (link.style.animation = 'navLinkFade 0.5s ease forwards 0.3s');
-        });
-    }
     handleClick() {
-        this.navList.classList.toggle(this.activeClass);
-        this.animateLinks();
+        console.log(this);
     }
     addClickEvent() {
-        this.mobileMenu.addEventListener("click", this.handleClick);
+        this.mobileMenu.addEventListener("click" , this.handleClick);
     }
 
     init() {
